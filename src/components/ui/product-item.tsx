2f38e4edcb96f356de2693e8ad744bd666198c8f
paulopes1 <paulolopesplj@gmail.com>
2023-10-24 21:02:50 -0300

    feat: add ProductItem and ProductList components

---
+++ src/components/ui/product-item.tsx
@@ -0,0 +1,57 @@
+import {ProductWithTotalPrice} from "@/helpers/product"
+import { ArrowDownIcon } from "lucide-react";
+import { Badge } from "./badge"
+import Image from "next/image";
+
+interface ProductItemProps{
+    product: ProductWithTotalPrice;
+}
+
+const ProductItem = ({product}: ProductItemProps) => {
+    return <div className="flex max-w-[156px] flex-col gap-4">
+        <div className=" relative bg-accent rounded-lg h-[150px] w-[156px] flex items-center justify-center ">
+            <Image
+                src={product.imageUrls[0]}
+                height={0}
+                width={0}
+                sizes="100vw"
+                className="h-auto max-w-[80%] w-auto max-h-[70%]" 
+                style={{
+                    objectFit: "contain"
+                }}
+                alt={product.name}
+            />
+
+            {product.discountPercentage > 0 &&(
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+                <ArrowDownIcon  size={14}/> {product.discountPercentage}%
+            </Badge>
+            )}
+        </div>
+
+        
+
+        <div className="flex flex-col gap-1"> 
+            <p className="w-full overflow-hidden text-sm text-ellipsis whitespace-nowrap">
+                {product.name}
+            </p>
+
+            <div className="flex items-center gap-2">
+                {product.discountPercentage > 0 ?(
+                    <>
+                        <p className="font-semibold">R$ {product.totalPrice.toFixed(2)}</p>
+
+                        <p className="opacity-75 line-through text-xs">
+                            R$ {Number(product.basePrice).toFixed(2)}</p>
+                    </>
+                ) : (
+                    <p className="font-semibold text-sm">R$ {product.totalPrice.toFixed(2)}</p>
+                )}
+
+
+            </div>
+        </div>
+    </div>
+};
+ 
+export default ProductItem;
